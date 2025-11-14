f3c04e766b780de692dc8128a3295e4e99581452
nasser-tarek <nasser.tarek.n1@@gmail.com>
2025-11-14 10:56:01 +0200

    edited

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 command -v docker >/dev/null || { echo "docker is not installed"; exit 1; }
 
-echo "🚀 Building and starting stack..."
+echo " Building and starting stack..."
 docker compose up -d --build
 
-echo "⏳ Waiting for web to be healthy..."
+echo "Waiting for web to be healthy..."
 docker compose ps
-echo "📡 Live logs :"
+echo "Live logs :"
 docker compose logs -f web
